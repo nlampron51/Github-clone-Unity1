7e1107a390294b1ea7f6393519c34618158f2527
lamprono51 <nlampron@cscmonavenir.ca>
2023-06-15 08:31:58 -0400

    nouveauté test2

--- Github-clone-unity1/Assets/Scenes/NewBehaviourScript1.cs
+++ Github-clone-unity1/Assets/Scenes/NewBehaviourScript1.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Microsoft
-public class NewBehaviourScript : MonoBehaviour
+
+public class NewBehaviourScript1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
